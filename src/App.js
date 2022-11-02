774d114300d36db73582f6fceab1b03e711de76c
StefiThe Bog <stefandrazicstefan@gmail.com>
2022-11-02 20:58:43 +0100

    general structure with borders on comp elements

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCurrencyComponent from "./SingleCurrencyComponent";
+import axios from "axios";
+
+/*https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=80&page=1&sparkline=false*/
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=80&page=1&sparkline=false"
+      )
+      .then((res) => setCoins(res.data))
+      .catch((err) => console.log(err));
+    console.log(coins);
+  }, []);
+  const queryCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(query.toLowerCase())
+  );
   return (
     <div className="app">
       <div className="search_component">
         <span>Search Cryptocurrency</span>
-        <form action="">
+        <form
+          action=""
+          onSubmit={(e) => e.preventDefault()}
+        >
           <input
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
             type="text"
             placeholder="Search"
           />
         </form>
       </div>
       <div className="results_component">
-        <SingleCurrencyComponent />
+        {queryCoins.map((coin) => {
+          return (
+            <SingleCurrencyComponent
+              key={coin.id}
+              image={coin.image}
+              name={coin.name}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              high={coin.high_24h}
+              low={coin.low_24h}
+              change={coin.price_change_percentage_24h}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/SingleCurrencyComponent.js
+++ src/SingleCurrencyComponent.js
@@ -6,21 +6,45 @@ function SingleCurrencyComponent({
   name,
   symbol,
   price,
-  volume,
+  high,
+  low,
+  change,
 }) {
+  const change_ = (change) => {
+    return change > 0
+      ? (change = `+${change.toFixed(3)}`)
+      : change.toFixed(3);
+  };
+  const giveColor = (data) => {
+    return data < 0 ? "red" : "green";
+  };
   return (
-    <div className="component">
-      <div className="coin_basic_info">
-        <img
-          src={image}
-          alt=""
-        />
-        <h1 className="coin_name">{name}</h1>
-        <p className="coin_symbol">{symbol}</p>
-      </div>
-      <div className="coin_data">
-        <p className="coin_price">${price}</p>
-        <p className="coin_volume">${volume}</p>
+    <div className="wrapper">
+      <div className="component">
+        <div className="coin_basic_info">
+          <div className="img_name">
+            <img
+              src={image}
+              alt=""
+            />
+            <h1 className="coin_name">{name}</h1>
+          </div>
+
+          <p className="coin_symbol">{symbol.toUpperCase()}</p>
+          <div className="price">
+            <p className="coin_price">${price}</p>
+          </div>
+        </div>
+        <div className="coin_data">
+          <p className="coin_high">${high}</p>
+          <p className="coin_low">${low}</p>
+          <p
+            className="coin_change"
+            style={{ color: giveColor(change) }}
+          >
+            {change_(change)}%
+          </p>
+        </div>
       </div>
       <hr />
     </div>
